9ab32f0378820c6b5495ffa6464cf800853b0314
Piko Strom <hengkakada899@gmail.com>
2025-03-02 22:06:05 +0700

    get data from bun leang

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', HomeController::class, 'index');
 Route::get('/about', HomeController::class, 'about');
 Route::get('/contact', HomeController::class, 'contact');
 
-
-
 Route::get("/login", AuthController::class, 'login');
 Route::post('/login-execute', AuthController::class, 'store');
 
