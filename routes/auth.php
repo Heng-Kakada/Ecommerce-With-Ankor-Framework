de5bd55aae9d31100a58d38c4688e112a2c2c9d3
Piko Strom <hengkakada899@gmail.com>
2025-03-03 22:12:04 +0700

    modify route to use role for route

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,10 @@
 use AnkorFramework\App\Route\Route;
 use src\controllers\user\AuthController;
 use src\controllers\user\HomeController;
+use src\controllers\dashboard\DashboardController;
+use src\controllers\dashboard\DashBoardUserController;
+use src\controllers\dashboard\DashBoardProductController;
+use src\controllers\dashboard\DashBoardCategoryController;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/logout', AuthController::class, 'logout');
@@ -13,9 +17,38 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::middleware(['auth', 'manager', 'admin'])->group(function () {
+    Route::get('/admin', DashboardController::class, 'index');
 
+    //product route
+    Route::get('/admin/products', DashBoardProductController::class, 'index'); // list products
+    Route::get('/admin/products/{id}', DashBoardProductController::class, 'show'); // show product detail (view)
+    Route::get('/admin/products/create', DashBoardProductController::class, 'create'); // load create product view
+    Route::post('/admin/products/store', DashBoardProductController::class, 'store'); // store product to db
+    Route::get('/admin/products/edit/{id}', DashBoardProductController::class, 'edit'); //load update product view
+    Route::put('/admin/products/edit', DashBoardProductController::class, 'update'); // update existing product
+    Route::delete('/admin/products/delete', DashBoardProductController::class, 'destroy');
+
+
+    //category route
+    Route::get('/admin/categories', DashBoardCategoryController::class, 'index');
+    Route::get('/admin/categories/create', DashBoardCategoryController::class, 'create');
+    Route::post('/admin/categories/store', DashBoardCategoryController::class, 'store');
+    Route::get('/admin/categories/edit/{id}', DashBoardCategoryController::class, 'edit'); // {id}/edit
+    Route::put('/admin/categories/edit', DashBoardCategoryController::class, 'update');
+    Route::delete('/admin/categories/delete', DashBoardCategoryController::class, 'destroy');
+
+
+    Route::get('/admin/file_upload', DashboardController::class, 'file_upload');
+    Route::post('/admin/upload', DashboardController::class, 'upload');
 });
 
 Route::middleware(['auth', 'admin'])->group(function () {
-
+    //user route
+    Route::get('/admin/users', DashBoardUserController::class, 'index'); // list products
+    Route::get('/admin/users/{id}', DashBoardUserController::class, 'show'); // show product detail (view)
+    Route::get('/admin/users/create', DashBoardUserController::class, 'create'); // load create product view
+    Route::post('/admin/users/store', DashBoardUserController::class, 'store'); // store product to db
+    Route::get('/admin/users/edit/{id}', DashBoardUserController::class, 'edit'); //load update product view
+    Route::put('/admin/users/edit', DashBoardUserController::class, 'update'); // update existing product
+    Route::delete('/admin/users/delete', DashBoardUserController::class, 'destroy');
 });
--- routes/web.php
+++ routes/web.php
@@ -44,44 +44,9 @@ Route::get('/categories/{slug}', CategoryController::class, 'show');
 */
 Route::get('/cart', CartController::class, 'index');
 
-/* 
-    All admin Route
-*/
-
-Route::get('/admin', DashboardController::class, 'index');
-
-Route::get('/admin/file_upload', DashboardController::class, 'file_upload');
-Route::post('/admin/upload', DashboardController::class, 'upload');
 
 
-//product route
-Route::get('/admin/products', DashBoardProductController::class, 'index'); // list products
-Route::get('/admin/products/{id}', DashBoardProductController::class, 'show'); // show product detail (view)
-Route::get('/admin/products/create', DashBoardProductController::class, 'create'); // load create product view
-Route::post('/admin/products/store', DashBoardProductController::class, 'store'); // store product to db
-Route::get('/admin/products/edit/{id}', DashBoardProductController::class, 'edit'); //load update product view
-Route::put('/admin/products/edit', DashBoardProductController::class, 'update'); // update existing product
-Route::delete('/admin/products/delete', DashBoardProductController::class, 'destroy');
 
 
-//category route
-Route::get('/admin/categories', DashBoardCategoryController::class, 'index');
-Route::get('/admin/categories/create', DashBoardCategoryController::class, 'create');
-Route::post('/admin/categories/store', DashBoardCategoryController::class, 'store');
-Route::get('/admin/categories/edit/{id}', DashBoardCategoryController::class, 'edit'); // {id}/edit
-Route::put('/admin/categories/edit', DashBoardCategoryController::class, 'update');
-Route::delete('/admin/categories/delete', DashBoardCategoryController::class, 'destroy');
-
-//user route
-Route::get('/admin/users', DashBoardUserController::class, 'index'); // list products
-Route::get('/admin/users/{id}', DashBoardUserController::class, 'show'); // show product detail (view)
-Route::get('/admin/users/create', DashBoardUserController::class, 'create'); // load create product view
-Route::post('/admin/users/store', DashBoardUserController::class, 'store'); // store product to db
-Route::get('/admin/users/edit/{id}', DashBoardUserController::class, 'edit'); //load update product view
-Route::put('/admin/users/edit', DashBoardUserController::class, 'update'); // update existing product
-Route::delete('/admin/users/delete', DashBoardUserController::class, 'destroy');
-
-
-// Route::get('/products/{id}', ProductController::class, action: 'show');
 
 require_once "auth.php";
